bb38c51dfa46bcbc1c6319fadb78fcedff209ed1
Derek Wan <d.wan@icloud.com>
2023-05-13 20:22:28 +0800

    Finish lesson 14 (deploying to Vercel)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import Layout from "@/comps/Layout";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: any;
+  pageProps: any;
+}) {
   return (
     <Layout>
       <Component {...pageProps} />
