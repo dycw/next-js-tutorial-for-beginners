6ab1d431cb6a5bbfc83c6d8aea55f5cc6e214871
Derek Wan <d.wan@icloud.com>
2023-05-13 20:09:18 +0800

    Finish lesson 13 (fetching a single item)

--- pages/ninjas/[id].tsx
+++ pages/ninjas/[id].tsx
@@ -9,10 +9,21 @@ export const getStaticPaths = async () => {
   return { paths, fallback: false };
 };
 
-export default function Details() {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return { props: { ninja: data } };
+};
+
+export default function Details({ ninja }) {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 }
