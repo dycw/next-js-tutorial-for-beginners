82d0a2b717fe138ede4d1ab200d520ccf2317c53
Derek Wan <d.wan@icloud.com>
2023-05-13 17:40:51 +0800

    Finish lesson 8 (redirecting users) (#8)

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+    console.log("use effect ran");
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
